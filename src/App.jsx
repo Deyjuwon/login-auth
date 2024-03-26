dc1f1bed56deec4b988978dd490d1df962cd2632
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-03-26 21:16:47 +0100

    firestore db

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Auth from './components/Auth'
+import { db } from './config/firebase-config'
+import { getDocs, collection } from 'firebase/firestore'
 
 function App() {
+
+  const [movieList, setMovieList] = useState([])
+  const moviesCollectionRef = collection(db, 'movies')
+
+  useEffect (() => {
+    const getMovieList  = async () =>{
+      try {
+        const data = await getDocs(moviesCollectionRef);
+        const filteredData = data.docs.map((doc) => ({...doc.data, id:doc.id}))
+        console.log(data)
+      } catch (err) {
+        console.error(err)
+      }
+    };
+    getMovieList()
+  }, [])
+ 
+
   return (
     <div>
       <Auth />
